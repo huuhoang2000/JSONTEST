7cd84ebaed4d3b9c7fab4450f97a68d5cfaccf00
Huu Hoang <chinhhuu2000@gmail.com>
2022-12-05 23:33:22 -0700

    wip: submit button function and print

--- script.js
+++ script.js
@@ -2,19 +2,20 @@ const button = document.getElementsByClassName("btn")[0];
 
 const inputid = document.getElementById("IDinput").value;
 const inputresource = document.getElementById("RESinput").value;
-
-
+const out1 = document.getElementById("output1");
 
 button.addEventListener("click", activatebutton);
 
 function activatebutton() {
+  let value = 0;
   if (inputid !== null && inputresource !== null ) {
   const getByID = (inputresource, inputid) => {
-      const requestUrl = 'https://jsonplaceholder.typicode.com/${' + inputresource + '}/${' + inputid + '}';
+      const requestUrl = 'https://jsonplaceholder.typicode.com/' + inputresource + '/' + inputid;
       fetch(requestUrl)
         .then((response) => response.json())
         .then((json) => console.log(json));
   };
+    value = getByID;
   }
   else if (inputresource !== null && inputid === null) {
     const getAll = (resource) => {
@@ -24,5 +25,11 @@ function activatebutton() {
       .then(response => response.json())
       .then(json => console.log(json))
     }
+    value = getAll;
   }
+  out1.innerHTML = value;
 }
+
+// function print() {
+//   out1.innerHTML = value;
+// }
