8f3e5149aa9787bf0776eecb67d558b46a62994f
Huu Hoang <chinhhuu2000@gmail.com>
2022-12-09 07:00:33 -0700

    update: syntax for resource and ID printing

--- script.js
+++ script.js
@@ -1,40 +1,33 @@
 const button = document.getElementsByClassName("btn");
+const inputresource = document.getElementById("RESinput").value;
+
+button[0].addEventListener("click", activatebutton);
+//lấy giá trị mới nhất của html.
 
 const inputid = document.getElementById("IDinput").value;
-const inputresource = document.getElementById("RESinput").value;
 
 const getByID = (resource, id) => {
     const requestUrl = 'https://jsonplaceholder.typicode.com/' + resource + '/' + id;
     fetch(requestUrl)
-      .then((response) => {console.log(response.json())})
+      .then((response) => {return response.json()})
       .then((json) => console.log(json))
-      // console.log(response.json()); 
   };
 
-  button[0].addEventListener("click", activatebutton);
-
-  // const getAll = (resource) => {
-  //   const requestUrl = `https://jsonplaceholder.typicode.com/${resource}`;
+const getAll = (resource) => {
+    const requestUrl = `https://jsonplaceholder.typicode.com/${resource}`;
   
-  //   fetch(requestUrl)
-  //   .then(response => {console.log(response.json())})
-  //   .then(json => console.log(json))
-  // }
+    fetch(requestUrl)
+      .then(response => {return response.json()})
+      .then(json => console.log(json))
+  }
 
 function activatebutton() {
-  if (inputid !== null && inputresource !== null ) {
-    // getByID(inputresource, inputid );
-    console.log(getByID(inputresource, inputid ));
-    // value = getByID();
-  }
-  // else if (inputresource !== null && inputid === null) {
-  //   getAll(inputresource);
-  //   console.log(getAll(inputresource));
-  // }
-}
+  const inputid = document.getElementById("IDinput").value;
 
-// function print() {
-//   out1.innerHTML = value;
-// }
-// console.log(getByID('posts', 1));
-// console.log(getAll('comments'));
+  if (inputid !== null && inputresource !== null ) {
+    getByID(inputresource, inputid);
+  }
+  else if (inputresource !== null && inputid === null) {
+    getAll(inputresource);
+  }
+}
